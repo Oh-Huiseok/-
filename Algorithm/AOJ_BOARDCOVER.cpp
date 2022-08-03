fc3a79af31226e829e9ac6e29c9b2dec23f5f60d
Oh-Huiseok <rlatotquf45@naver.com>
2022-08-03 23:30:30 +0900

    220803

--- Algorithm/AOJ_BOARDCOVER.cpp
+++ Algorithm/AOJ_BOARDCOVER.cpp
@@ -10,6 +10,8 @@ typedef struct {
 	bool possible;
 } gamecover;
 
+int temp = 0;
+
 typedef struct {
 	int y, x;
 }block;
@@ -18,31 +20,20 @@ vector<int> ans;
 int score = 0;
 
 const block blocktypes[4][3] = {
-	{{0, 0}, {0, 1}, {0, 1}}, 
+	{{0, 0}, {1, 0}, {0, 1}}, 
 	{{0, 0}, {0, 1}, {1, 1}},
-	{{0, 1}, {1, 1}, {1, 0}},
-	{{0 ,0}, {1, 0}, {1, -1}}
+	{{0 ,0}, {1, 0}, {1, 1}},
+	{{0, 1}, {1, 1}, {1, 0}}
 };
 
-bool chk(gamecover g) { // 원래 덮혀있었으면 1, 덮었으면 -1, 안덮었으면 0
+bool chk(gamecover g) { // 원래 덮혀있었으면 1, 안덮었으면 0
 	for (int i = 0; i < g.y; i++) {
 		for (int j = 0; j < g.x; j++) {
 			if (g.mat[i][j] == 0)
-				return 0;
+				return false;
 		}
 	}
-	return 1;
-}
-
-int cancover(gamecover g, int y, int x) {
-	int cnt1, cnt2;
-	int c;
-	for (c = 0;  c< 4; c++) {
-		for (int q = 0; q < 3; q++) {
-			int posy = y + blocktypes[c][q].y;
-			int posx = x + blocktypes[c][q].x;
-		}
-	}
+	return true;
 }
 
 bool inrange(gamecover g, int y, int x) {
@@ -52,15 +43,51 @@ bool inrange(gamecover g, int y, int x) {
 		return false;
 }
 
-void boardcover(gamecover g, int y, int x) {
-	if (chk) {
+void print(gamecover g) {
+	for (int i = 0; i < g.y;  i++) {
+		for (int j = 0; j < g.x; j++) {
+			cout << g.mat[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+bool possible(gamecover g, int y, int x, int t) {
+	for (int i = 0; i < 3; i++) {
+		int py = y + blocktypes[t][i].y;
+		int px = x + blocktypes[t][i].x;
+
+		if (py < 0 || px < 0 || py >= g.y || px >= g.x || g.mat[py][px] == 1)
+			return false;
+	}
+	return true;
+}
+
+void boardcover(gamecover g, int y, int x) {//재귀함수 초반에 가장 가까운 0 찾는 알고리즘 추가하기
+	if (chk(g)) {
 		score++;
 		return;
 	}
-	if (!inrange(g, y, x))
-		return;
-	if (g.mat[y][x] == 1 || g.mat[y][x] == -1)
-		return;
+
+	int posy, posx;
+
+	for (int i = 0; i < 4; i++) {
+		if (possible(g, y, x, i)) {
+			for (int j = 0; j < 3; j++) {
+				posy = blocktypes[i][j].y + y;
+				posx = blocktypes[i][j].x + x;
+				g.mat[posy][posx] = 1;
+			}
+			print(g);
+			boardcover(g, y, x);
+			for (int j = 0; j < 3; j++) {
+				posy = blocktypes[i][j].y + y;
+				posx = blocktypes[i][j].x + x;
+				g.mat[posy][posx] = 0;
+			}
+		}
+	}
 }
 
 int main() {
@@ -68,7 +95,7 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int c;
+	int c, cnt;
 	cin >> c;
 
 	vector<gamecover> games(c);
@@ -78,14 +105,16 @@ int main() {
 		cin >> y >> x;
 		games[i].x = x;
 		games[i].y = y;
+		games[i].mat.resize(y, vector<int>(x));
+		games[i].possible = true;
 
-		char temp;
-		int cnt = 0;
+		string temp;
+		cnt = 0;
 
 		for (int j = 0; j < y; j++) {
-			for (int k = 0; k < x; k++) {
-				cin >> temp;
-				if (temp == '#')
+			cin >> temp;
+			for (int k = 0; k < temp.length(); k++) {
+				if (temp[k] == '#')
 					games[i].mat[j][k] = 1;
 				else {
 					games[i].mat[j][k] = 0;
@@ -99,13 +128,22 @@ int main() {
 	}
 
 	for (int i = 0; i < c; i++) {
-		if (games[i].possible == false)
+		if (games[i].possible == false) {
 			ans.push_back(0);
+		}
 		else {
-			boardcover(games[i], 0, 0);
+			for (int j = 0; i < games[i].y; j++) {
+				for (int k = 0; j < games[i].x; k++) {
+					boardcover(games[i], j, k);
+				}
+			}
 			ans.push_back(score);
 		}
 		score = 0;
+	}
+
+	for (int x : ans) {
+		cout << x << endl;
 	}
 	return 0;
 }
