e667fac94c2a7c1b74a62580943a0112bfb1f2df
Oh-Huiseok <rlatotquf45@naver.com>
2022-09-24 12:49:51 +0900

    commit

--- KNU_hackerthon_2022/makeCsv.py
+++ KNU_hackerthon_2022/makeCsv.py
@@ -2,6 +2,8 @@
 # pip install lxml
 # pip install geopy
 # pip install xmltodict
+# pip install googlemaps
+# pip install Pillow
 
 from logging import error
 import requests
@@ -15,21 +17,28 @@ import googlemaps
 import xmltodict
 
 googlemaps_key = "AIzaSyCWpyN2lFq3QU71QEDUVzNRiemE_LI58Co"
-gmaps = googlemaps.Client(key=googlemaps_key)
+maxwidth = 400
+maxheight = 400
 
+print("Process initiating")
+gmaps = googlemaps.Client(key=googlemaps_key)
 
 decodingKey = "obvb6zpR5qgYzRB5yjXWoHY+/zhFKzZ7rVuItJKMVKU+5BgtGwgwuTbqVAUZLyWEVxqAivJ/DdBNiGAvR+3lPQ=="
 url = 'http://apis.data.go.kr/6270000/getTourKorAttract/getTourKorAttractList'
-params = {'serviceKey': decodingKey, 'numOfRows':225}
+params = {'serviceKey': decodingKey, 'numOfRows': 255}
 response = requests.get(url, params=params)
 soup = BeautifulSoup(response.content, 'lxml-xml')
 items = soup.find_all("item")
+
+
 '''
 data_dict = open('data.xml', 'r', encoding='utf-8-sig')
 soup = BeautifulSoup(data_dict, 'lxml')
-fullcnt = soup.find("totalCount")'''
+fullcnt = soup.find("totalCount")
+'''
 
 row = []
+
 
 def parse(item, i):
     try:
@@ -40,44 +49,65 @@ def parse(item, i):
             CONTENT = CONTENT[:500] + '...중략'
         ADDRESS = item.find("address").get_text()
 
-        geo_location = gmaps.geocode(ADDRESS)[0].get('geometry')
-        test = geo_location['location']
-        LAT = geo_location['location']['lat']
-        LON = geo_location['location']['lng']
-
         PAGE_URL = item.find("homepage").get_text()
         TEL = item.find('tel').get_text()
         HOUR = item.find('attr01').get_text()
 
+        
+        geo_location = gmaps.geocode(ADDRESS)[0].get('geometry')
+        test = geo_location['location']
+        LAT = geo_location['location']['lat']
+        LON = geo_location['location']['lng']
+        COORDINATE = [LAT, LON]
+
+        place_result = gmaps.geocode(NAME)[0]
+        #print(place_result)
+        my_place_id = place_result['place_id']
+        #print(my_place_id)
+        place_details = gmaps.place(place_id=my_place_id, fields=['photo'])
+        photo_id = place_details['result']['photos'][0]['photo_reference']
+        raw_image_data = gmaps.places_photo(photo_reference=photo_id, max_height=maxheight,
+                                            max_width=maxwidth)
+        #print(raw_image_data)
+        filename = "images/%d.jpg" % i
+        
+        print(filename + " create")
+        f = open(filename, 'wb')
+
+        for chunk in raw_image_data:
+            if chunk:
+                f.write(chunk)
+
+        f.close()
+        
         return {
             "id": ID,
             "name": NAME,
-            "content": CONTENT,
-            "address": ADDRESS,
+            "contents": CONTENT,
             "homepage": PAGE_URL,
+            "address": ADDRESS,
             "tel": TEL,
             "hour": HOUR,
-            "lat": LAT,
-            "lon": LON
+            "coordinate" : COORDINATE
         }
     except:
-        print("error")
         return {
             "id": None,
             "name": None,
-            "content": None,
-            "address": None,
+            "contents": None,
             "homepage": None,
+            "address": None,
             "tel": None,
             "hour": None,
-            "lat": None,
-            "lon": None,
+            "coordinate" : None
         }
 
-for (i,item) in enumerate(items):
+
+for (i, item) in enumerate(items):
     row.append(parse(item, i))
 
 df = pd.DataFrame(row)
 
 df.to_csv("테스트 데이터셋.csv", index=False, mode='w', encoding="utf-8-sig")
-
+print(".csv file create")
+print('Program shut down')
